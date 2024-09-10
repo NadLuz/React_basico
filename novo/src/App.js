2258683c111469177eb015b26a3417dd9da4fe76
Nadja Luz <nadja.pluz@gmail.com>
2024-09-10 19:04:10 -0300

    Primeiro programa

---
+++ novo/src/App.js
@@ -0,0 +1,14 @@
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo React!</h1>
+      <Button title = "Entrar"/>
+      <Button title = "Fechar"/>
+      <Button title = "Salvar"/>
+    </div>
+  );
+}
+
+export default App;
