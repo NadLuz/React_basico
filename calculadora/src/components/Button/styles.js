e62ae9ca2399ad1398921ef693b3afc0408ef8fc
Nadja Luz <nadja.pluz@gmail.com>
2024-09-10 21:18:13 -0300

    calculadora em andamento

---
+++ calculadora/src/components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #708090;
+    background-color: #B0C4DE;
+    color: white;
+    font-size:30px;
+    font-weight: bold;
+    flex:1;
+    &:hover {
+        opacity:0.6;
+    }
+`
---
+++ calculadora/src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #A9A9A9;
+    border-radius: 8px 8px 0 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family:'Roboto';
+    
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #A9A9A9;
+        border: 0;
+        border-radius: 8px 8px 0 0;
+        text-align: right;
+        padding: 0 10px;
+
+        font-size:24px;
+        font-family:'Roboto';
+        color: #2F4F4F;
+    }
+`
